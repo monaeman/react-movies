56c9b4559c58dbd8d0148967c55c92256aa3d331
monaeman <mona.eman2018@gmail.com>
2023-08-01 05:31:04 -0400

    styling

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 //import styled from 'styled-components'
 import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-image: url("https://kettleheroes.com/wp-content/uploads/2022/02/Why-is-Popcorn-a-Movie-Food.jpg"); 
+    background-image: url("https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"); 
     background-size: cover;
     background-position: right;
     background-repeat: no-repeat;
@@ -22,14 +22,14 @@ const FormContainer = styled.div`
   display: flex;
   font-size : 10px;
   flex-direction: column;
-  align-items: center;
+  align-items: right;
   padding: 20px;
   max-width: 700px;
   margin: 0 auto;
   margin-top: 50px;
  
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-  position: sticky;
+  
  
   
   
@@ -47,20 +47,21 @@ const FormContainer = styled.div`
 
   `;
 
-  
-  
 
 
 
 
 
-  
+
+
+
 
 
 const Title = styled.h1`
-  font-size: 80px;
+  font-size: 70px;
   margin-bottom: 5px;
   text-align: center;
+  
   
 `;
 
@@ -74,7 +75,7 @@ const SubmitButton = styled.input`
   font-size: 35px;
   border-radius: 7%;
   align-item: center;
-
+  
 `;
 
 
@@ -100,10 +101,10 @@ function Form({ movieSearch }) {
     <>
       <GlobalStyle />
       <FormContainer>
-      
+
         <Title>Search for a Movie</Title>
         <form onSubmit={handleSubmit}>
-        
+
           <input
             type="text"
             value={form.searchTerm}
--- src/component/MovieDisplay.jsx
+++ src/component/MovieDisplay.jsx
@@ -3,18 +3,22 @@ import styled from "styled-components";
 //import styled, { createGlobalStyle } from 'styled-components';
 
 const Title = styled.h1`
-  font-size: 100px;
-  position: sticky;
-  top:0;
+  font-size: 200px;
+  
+  color : purple;
+ 
 `;
 
 const Image = styled.img
-`
+  `
 
 height : 1100px;
-width : 1000px;
+width : 800px;
+
 position: sticky;
-top: 0;
+top:0;
+
+
 
 `
 const MovieContainer = styled.div`
@@ -22,11 +26,10 @@ const MovieContainer = styled.div`
 
 h3{
 
-    font-size: 60px;
-    position: sticky;
-    top:0;
-    color: white;
-    text-align: center;
+    font-size: 70px;
+    color: gold;
+    text-align: ;
+    
 }
 
 `
@@ -36,17 +39,24 @@ function MovieDisplay({ movie }) {
   const loaded = () => {
     return (
       <>
-      <MovieContainer>
-      <Title>{movie.Title}</Title>
-        <Image src={movie.Poster} alt={movie.Title} />
-        <h3>Genre :{movie.Genre}</h3>
-        <h3>Year :{movie.Year}</h3>
-        
-        <h3>Actors :{movie.Actors}</h3>
+        <MovieContainer>
+          <Title>{movie.Title}</Title>
+          <Image src={movie.Poster} alt={movie.Title} />
+          <h3>Genre :{movie.Genre}</h3>
+          <h3>Year :{movie.Year}</h3>
+          <h3>Rated: {movie.Rated}</h3>
+          <h3>Actors :{movie.Actors}</h3>
+          <h3>Director: {movie.Director}</h3>
+          <h3>Runtime: {movie.Runtime}</h3>
+          <h3>Writer: {movie.Writer}</h3>
+          <h3>Plot: {movie.Plot}</h3>
+          <h3>Awards : {movie.Awards}</h3>
 
-        
-       </MovieContainer>
-        
+
+
+
+        </MovieContainer>
+
       </>
     );
   };
